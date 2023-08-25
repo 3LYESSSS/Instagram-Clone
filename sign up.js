e65fc14e39ff0473a02cae33b5a48b3dce6d7dd2
ELYES JRIDI <120260650+3LYESSSS@users.noreply.github.com>
2023-08-25 12:05:50 +0100

    Add files via upload

---
+++ sign up.js
@@ -0,0 +1,25 @@
+function ImagineGrand(y) {
+    y.style.height = "200px";
+    y.style.width = "200px";
+}
+
+function ImageNormal(y) {
+    y.style.height = "100px";
+    y.style.width = "100px";
+}
+
+function verif() {
+
+}
+
+//resetting
+function reset() {
+    document.getElementById('name').value="";
+    document.getElementById('firstname').value="";
+    document.getElementById('mail').value="";
+    document.getElementById('passw').value="";
+    document.getElementById('days').value="";
+    document.getElementById('months').value="";
+    document.getElementById('years').value="";
+    document.getElementById('oc').value="";
+}
